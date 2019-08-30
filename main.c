2725e62a38ca19fbbd47dcfa0215c7c826256665
Alzero7 <jessar.alzero7@gmail.com>
2019-08-30 09:21:43 -0500

    Practica4

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// Actividad 1 //
 
-int main(int argc, char *argv[]) {
+int main() 
+{
+	system("color 0B");
+	int a;
+	int b;
+	
+	printf("\n\n\t Ingrese un numero \n\n\t ");
+	scanf("%d",&a);
+	printf("\n\t Ingrese otro numero \n\n\t ");
+	scanf("%d",&b);
+	
+	if (a==b)
+		printf("\n\n\t Los numeros %d y %d son iguales",a,b);
+	else
+		if (a<b)
+			printf("\n\n\t El numero mayor es: %d",b);
+		else
+			printf("\n\n\t El numero mayor es: %d",a);
+			 
+			 
+
+	// Actividad 2 //
+	
+	
+	
 	return 0;
 }
+
+
+
