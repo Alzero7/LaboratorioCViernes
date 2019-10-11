43d11bffe1337733f59037f35e8013945368d3bc
Alzero7 <jessar.alzero.7@gmail.com>
2019-10-11 08:48:43 -0500

    Practica10

--- main.c
+++ main.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+	Jesus Armando Espino Rodriguez
+	11/10/2019
+	006
+	Viernes 7:00am - 9:00am
+	1844607
+*/
 
-int main(int argc, char *argv[]) {
-	return 0;
+//  Actividad 1  //
+
+/*	Se prentende crear una aplicación que pida una cadena de texto por teclado y verifique si
+ dicha cadena es palindromo. */
+
+int main()
+{	
+		system("color 0B");
+		char Text[20];
+		char Ntext[20];
+		int i;
+		int x;
+		char y=' ';
+		int s=0;
+		int cy=0;
+		int n=1;
+		
+		printf("\n\t Introduzca un texto, por favor: \n\n\t ");
+		gets(Text);
+		x=strlen(Text);
+		
+		for(i=0;i<=x;i++)
+		{
+				if(Text[i]==y)
+				{
+					cy++;
+					i++;
+				}
+					Ntext[s]=Text[i];
+					s++;
+		}
+		
+		i=0;
+		s=0;
+		for(i=x-cy;i>=0;i--)
+		{
+			if(Ntext[i]==Ntext[s])
+			{
+				n++;
+			}
+			s++;
+		}
+		
+		if(n==x)
+		{
+			printf("\n\t El texto es palindromo");
+		}
+		else
+		{
+			printf("\n\t El texto no es palindromo");
+ 	}
+ 
+ 	
+ 	//  Actividad 2  //
+ 	
+ 	/*  Escribir un programa que, dado un array de 10 caracteres (introducidos a mano en el
+  código), calcule cuántas letras aparecen y cuantas hay de cada una
+  
+		Ejemplo.
+  
+  AABBAA BBAA AAABBBV
+  ? A:9
+  ? B:7
+  ? V:1
+		*/ 	
+ 	
+ 	int word[10];
+ 	
+ 	
+		return 0;
 }
