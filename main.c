658b2ded88a84630c2ca8a4dabfda0600a2e38fe
Alzero7 <jessar.alzero.7@gmail.com>
2019-10-11 23:06:13 -0500

    Practica10

--- main.c
+++ main.c
@@ -18,66 +18,131 @@
 int main()
 {	
 		system("color 0B");
-		char Text[20];
-		char Ntext[20];
-		int i;
-		int x;
-		char y=' ';
-		int s=0;
-		int cy=0;
-		int n=1;
+		char text[20];
+		char ntext[20];
+		int i,k=0;
+		char spa=' ';
+		int cspa=0;
+		int t;
+		int nt=0;
 		
-		printf("\n\t Introduzca un texto, por favor: \n\n\t ");
-		gets(Text);
-		x=strlen(Text);
+		printf("\n\t Ingrese el texto, por favor: \n\n\t ");
+		gets(text);
+		t=strlen(text);
 		
-		for(i=0;i<=x;i++)
+		for(i=0;i<=t;i++)
 		{
-				if(Text[i]==y)
-				{
-					cy++;
-					i++;
-				}
-					Ntext[s]=Text[i];
-					s++;
+			if(text[i]==spa)
+			{
+				cspa++;
+				i++;
+			}
+			ntext[k]=text[i];
+			k++;
 		}
 		
 		i=0;
-		s=0;
-		for(i=x-cy;i>=0;i--)
+		k=0;
+		t=strlen(ntext);
+		for(i=t-1;i>=0;i--)
 		{
-			if(Ntext[i]==Ntext[s])
+			if(ntext[i]==ntext[k])
 			{
-				n++;
+				nt++;
 			}
-			s++;
+			k++;
 		}
 		
-		if(n==x)
-		{
-			printf("\n\t El texto es palindromo");
+		 if (nt==t)
+      	{
+		  	printf("\n\t La palabra es palindrome  \n\n\t ");
 		}
 		else
 		{
-			printf("\n\t El texto no es palindromo");
- 	}
+      		printf ("\n\t La palabra no es palindrome \n\n\t ");
+    	}
+		system("pause");
+		system("cls");
+		
  
  	
- 	//  Actividad 2  //
+ 		//  Actividad 2  //
  	
- 	/*  Escribir un programa que, dado un array de 10 caracteres (introducidos a mano en el
-  código), calcule cuántas letras aparecen y cuantas hay de cada una
+ 		/*  Escribir un programa que, dado un array de 10 caracteres (introducidos a mano en el
+  		código), calcule cuántas letras aparecen y cuantas hay de cada una
   
 		Ejemplo.
   
-  AABBAA BBAA AAABBBV
-  ? A:9
-  ? B:7
-  ? V:1
+ 		 AABBAA BBAA AAABBBV
+ 		 ? A:9
+ 		 ? B:7
+ 		 ? V:1
 		*/ 	
  	
- 	int word[10];
+ 		char word[10]="hola teo";
+ 		int cl=0;
+ 		int nl=0;
+ 		cl=strlen(word); 
+ 		int b=strchr("h",word);
+ 		
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b==-1)
+ 			nl++;
+		}
+ 		printf("\n\t h= %d",nl);
+ 		
+ 		nl=0;
+ 		b=strchr("o",word);
+
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b==-1)
+ 			nl++;
+		}
+ 		printf("\n\t o= %d",nl);
+ 		
+ 		nl=0;
+ 		b=strchr("l",word);
+ 	
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b==-1)
+ 			nl++;
+		}
+ 		printf("\n\t l= %d",nl);
+ 		
+ 		nl=0;
+ 		b=strchr("a",word);
+ 	
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b!=0)
+ 			nl++;
+		}
+ 		printf("\n\t a= %d",nl);
+ 		
+ 		nl=0;
+ 		b=strchr("t",word);
+ 		
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b==-1)
+ 			nl++;
+		}
+ 		printf("\n\t t= %d",nl);
+ 		
+ 		nl=0;
+ 		b=strchr("e",word);
+ 		
+ 		for(i=0;i<cl;i++)
+ 		{
+ 			if(b==-1)
+ 			nl++;
+		}
+ 		printf("\n\t e= %d",nl);
  	
  	
-		return 0;
+ 	
+	return 0;
 }
